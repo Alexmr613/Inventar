2c228d86939a1f13f2877761991be4fce4585ac3
valentina villarreal <valvilla71717@gmail.com>
2026-03-13 18:46:27 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Inventario.py
+#ESTE Programa funciona mediante validaciones por el bucle while, mientras no entre el tipo de dato correcto el programa.
+te lo indicara y te exigira nuevamente el dato correcto
---
+++ inventario.py
@@ -0,0 +1,12 @@
+
+while True :
+    try:
+    
+            
+      precio = float(input("Digite su precio " )) # Validacion precio
+      break
+
+    except ValueError :
+        print("Por favor ingrese un num valido")
+
+
