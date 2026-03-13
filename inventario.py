c80ccb68a1f2423a1382c2e7abf567671de65ce6
valentina villarreal <valvilla71717@gmail.com>
2026-03-13 18:56:16 -0500

    primer commit

--- inventario.py
+++ inventario.py
@@ -9,4 +9,28 @@ while True :
     except ValueError :
         print("Por favor ingrese un num valido")
 
+while True : 
+    try :
+ 
+      nombre = str(input("Digite el nombre del producto:\n ")) # Validacion nombre
+      break
+    except ValueError :
+        print("Por favor ingrese un nombre valido")
+
+
+while True : 
+    try:
+    
+     cantidad = int(input("Digite la cantidad a ingresar ")) # Validacion cantidad
+     break
+    except ValueError:
+       print("Digite una cantidad valida")
+costototal= (precio * cantidad)
+
+print("Producto ", nombre, "Precio " , precio , "Cantidad ", cantidad, "Total " , costototal) # Muestra el valor total y las caracteristicas del producto
+
+
+#ESTE Programa funciona mediante validaciones por el bucle while, mientras no entre el tipo de dato correcto el programa te lo indicara y te exigira nuevamente el dato correcto
+
+
 
